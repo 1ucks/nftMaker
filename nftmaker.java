39010b7838b25cdd89ccc0e7d2aa6b5466f4bb07
Lux <luxwooten@gmail.com>
2022-09-04 22:03:56 -0700

    added os detection for chrome location & execution

--- nftmaker.java
+++ nftmaker.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.awt.AWTException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
@@ -8,10 +9,29 @@ import java.awt.event.KeyEvent;
 import java.awt.datatransfer.StringSelection;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
+import java.util.Scanner;
 public class nftmaker{
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, AWTException, InterruptedException {
+		
+		Scanner in = new Scanner(System.in);
+		
 		//declarations
 		String randLine = ""; 
+		String chromePath = "";
+
+
+		//determines what kind of computer u are using and sets the chrome file path
+		if(System.getProperty("os.name").toLowerCase().startsWith("windows")){
+            chromePath = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe";
+			System.out.println("Detected windows");
+        }else if(System.getProperty("os.name").toLowerCase().startsWith("linux")){
+			chromePath = "/opt/google/chrome/chrome";
+			System.out.println("Detected linux");
+		}else{
+			System.out.print("Please enter the location of chrome.exe on your computer: ");
+			chromePath = in.nextLine();
+		}
+
 
 		
 		//counts number of lines in da file
@@ -27,7 +47,7 @@ public class nftmaker{
 		    } catch (Exception e) {
 		      e.getStackTrace();
 		    }
-		//randomly finds a line to look read for a word
+		//randomly finds a line to read for a word
 		int lineNum = (int) (Math.random()*count)+1;
         try (Stream<String> lines = Files.lines(Paths.get("words.txt"))) {
             randLine = lines.skip(lineNum).findFirst().get();
@@ -40,12 +60,25 @@ public class nftmaker{
         Robot robot = new Robot();
 		StringSelection selection = new StringSelection(randLine);
 		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		//copies the word
 		clipboard.setContents(selection, selection);
+		
+
+		//opens google(hopefully)
+
+		Runtime run = Runtime.getRuntime();
+		run.exec(chromePath);
+
+		//pastes
 		robot.delay(5000);
 		robot.keyPress(KeyEvent.VK_CONTROL);
 		robot.keyPress(KeyEvent.VK_V);
 		robot.keyRelease(KeyEvent.VK_V);
 		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.delay(100);
+		robot.keyPress(10);
+		robot.keyRelease(10);
+
       } catch (Exception e) {
       }
 	  
